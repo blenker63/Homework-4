bb16d1c6f48b3a871976d9f2d80ef0c00a91b221
vaceslavkameronov <slka@mail.ru>
2023-01-09 22:58:29 +0400

    решение 1й задачи

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int humanAge = 50;
+        if (humanAge >= 18); {
+            System.out.println("Если возраст человека равен " + humanAge + " лет, то он совершеннолетний.");
+        }
+        if (humanAge < 18); {
+            System.out.println("Если возраст человека равен " + humanAge + " лет, то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+    }}
