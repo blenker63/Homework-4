5cacb206533ec1903e969c0c241d2d83b8288639
vaceslavkameronov <slka@mail.ru>
2023-01-10 21:30:55 +0400

    решение 4й задачи

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -45,4 +46,22 @@ public static void task2(){
         } else  {
             System.out.println( "Если скорость " + speed2 + " км/ч, то можно ездить спокойно.");
         }}
-    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int humanAge = 50;
+        boolean needToGoToKindergarten = humanAge >= 2 && humanAge <=6;
+        boolean needToGoToSchool = humanAge >=7 && humanAge <18;
+        boolean needToGoToUniversity = humanAge >=18 && humanAge <24;
+        boolean needToGoToWork = humanAge >=24;
+        if (needToGoToKindergarten) {
+            System.out.println("Если возраст человека равен " + humanAge + " лет, то ему нужно ходить в детский сад.");
+        } else {
+            if (needToGoToSchool) {
+            System.out.println("Если возраст человека равен " + humanAge + " лет, то ему нужно ходить в школу.");
+        } else {
+                if (needToGoToUniversity) {
+            System.out.println("Если возраст человека равен " + humanAge + " лет, nо ему нужно ходить в университет.");
+        } else {
+                    System.out.println("Если возраст человека больше " + humanAge + " лет, то ему нужно ходить на работу.");
+                }
+    }}}}
