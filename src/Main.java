9797c8b8c4535ae68e97c36e75bb314cf6ccfe49
vaceslavkameronov <slka@mail.ru>
2023-01-10 08:25:23 +0400

    решение 1й задачи

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
         int humanAge = 50;
-        if (humanAge >= 18); {
+        if (humanAge >= 18) {
             System.out.println("Если возраст человека равен " + humanAge + " лет, то он совершеннолетний.");
         }
-        if (humanAge < 18); {
+        if (humanAge < 18) {
             System.out.println("Если возраст человека равен " + humanAge + " лет, то он не достиг совершеннолетия, нужно немного подождать.");
         }
-    }}
+    }
+public static void task2(){
+    System.out.println("Задача 2");
+    int airTemperature = 10;
+    if (airTemperature <5)
+    {
+        System.out.println("одень шапку");
+    } else {
+        System.out.println("не надевай");
+    }
+
+}}
