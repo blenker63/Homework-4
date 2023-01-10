5b971b10857527281db1467c7ee93fb4fed3b4b3
vaceslavkameronov <slka@mail.ru>
2023-01-10 10:19:19 +0400

    решение 2й задачи

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,19 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int humanAge = 50;
+        int humanAge = 5;
         if (humanAge >= 18) {
             System.out.println("Если возраст человека равен " + humanAge + " лет, то он совершеннолетний.");
         }
         if (humanAge < 18) {
             System.out.println("Если возраст человека равен " + humanAge + " лет, то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+        int humanAge2 = 50;
+        if (humanAge2 >= 18) {
+            System.out.println("Если возраст человека равен " + humanAge2 + " лет, то он совершеннолетний.");
+        }
+        if (humanAge2 < 18) {
+            System.out.println("Если возраст человека равен " + humanAge2 + " лет, то он не достиг совершеннолетия, нужно немного подождать.");
         }
     }
 public static void task2(){
@@ -19,9 +26,9 @@ public static void task2(){
     int airTemperature = 10;
     if (airTemperature <5)
     {
-        System.out.println("одень шапку");
+        System.out.println("На улице холодн, нужно надеть шапку.");
     } else {
-        System.out.println("не надевай");
+        System.out.println("Сегодня тепло, можно идти без шапки.");
     }
 
 }}
